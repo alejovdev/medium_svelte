1f32b1e216768c19d8caa8b2c087b5b7f35cb2ab
Alejov <email.alejov@gmail.com>
2020-02-02 01:06:41 -0500

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+  export let dark;
+
+  function handleToggle() {
+    dark = !dark;
+  }
+</script>
+
+<style>
+  .container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .toggle {
+    border: none;
+    outline: none;
+    border-radius: 5px;
+    padding: 10px;
+    color: white;
+  }
+  .toggle:hover {
+    cursor: pointer;
+  }
+  .dark {
+    background-color: #3b5998;
+    color: white;
+  }
+  .light {
+    background-color: #ffffff;
+    color: black;
+  }
+</style>
+
+<div class="container {dark ? 'dark' : 'light'}">
+  <button class="toggle {dark ? 'light' : 'dark'}" on:click={handleToggle}>
+    Toggle background
+  </button>
+</div>
